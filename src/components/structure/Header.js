51bd5aadc881b39a6fabea90db94c028aca7af64
varshikchebrolu <varshikchebrolu@gmail.com>
2023-06-19 00:35:12 -0600

    removed unnecessary imports and formatting

--- src/components/structure/Header.js
+++ src/components/structure/Header.js
@@ -2,20 +2,17 @@ import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import { Image } from "@mui/icons-material";
-import { Avatar } from "@mui/material";
 import homeLogo from "../../utils/main_logo.jpeg"
 
 export default function Header() {
     return (
-        <Box sx={{color:"rgb(242, 247, 239)"}}>
+        <Box sx={{ color: "rgb(242, 247, 239)" }}>
             <AppBar psotion="static">
-                <Toolbar sx={{justifyContent:'space-between',backgroundColor:'black'}}>
-                    
+                <Toolbar sx={{ justifyContent: 'space-between', backgroundColor: 'black' }}>
+
                     <Box>
-                        <img alt="Main_logo" src={homeLogo} style={{maxWidth:'20%'}}/>
+                        <img alt="Main_logo" src={homeLogo} style={{ maxWidth: '20%' }} />
                     </Box>
                     <Box>
                         <Button color="inherit"> Home </Button>
