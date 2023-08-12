b3b05c94e0c3611a04e4782b36239da6975c4995
varshik chebrolu <varshikchebrolu@gmail.com>
2023-08-12 15:05:57 -0600

    Added resume to download and view

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import "./App.css";
 import Content from "./components/structure/Content";
 import Header from "../src/components/structure/Header.js";
 import Footer from "../src/components/structure//Footer.js";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Resume from '../src/components/elements/Resume';
+
 
 function App() {
   return (
     <Box container sx={{
-      background: 'linear-gradient(180deg, #34a0a4 5%, #168aad 13%, #1a759f 60%, #1e6091 80%,#184e77 100%)'
+      background: 'linear-gradient(0deg, #34a0a4 5%, #168aad 13%, #1a759f 60%)'
     }}
     >
+      <Router>
       <Header />
-      <Content />
+      
+      <Routes>
+        <Route path='/' Component={Content}/>
+        <Route path="/resume" Component={Resume} />
+      </Routes>
+    </Router>
       <Footer />
     </Box>
   );
---
+++ src/components/elements/Resume.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { Box, Button } from "@mui/material";
+import { Document, Page } from "react-pdf";
+import resume from "../utils/resume.pdf";
+import { pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.js",
+  import.meta.url
+).toString();
+
+export default function Resume() {
+  const handleDownload = () => {
+    const filePath = "/resume.pdf";
+    const link = document.createElement("a");
+    link.href = process.env.PUBLIC_URL + filePath;
+    link.download = "VarshikChebrolu_Resume.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  return (
+    <Box
+      container
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        marginTop: "10vh",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "3vh",
+      }}
+    >
+      <Box>
+        <Button
+          color="success"
+          variant="contained"
+          sx={{
+            fontSize: "1rem",
+            fontWeight: "bold",
+            textTransform: "capitalize",
+          }}
+          onClick={handleDownload}
+        >
+          Download Resume
+        </Button>
+      </Box>
+      <Box
+        sx={{
+          marginBottom: "3vh",
+        }}
+      >
+        <Document file={resume}>
+          <Page pageNumber={1} renderTextLayer={false} />
+        </Document>
+      </Box>
+    </Box>
+  );
+}
